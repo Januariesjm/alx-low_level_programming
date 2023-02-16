715df8cb008d58c2c43bec20f81b7a7d9fc31d0a
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-02-16 01:35:39 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a string using put function
+ * Return: 0 (Printed sucessfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
