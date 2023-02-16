3f57fd59c86383433bd95978ba40d155ea624ed2
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-02-16 02:23:06 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main - A C program that prints a string using printf
- * Return: 0(success)
+ * main - Hello, printf
+ *
+ * Return: Always 0(Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
