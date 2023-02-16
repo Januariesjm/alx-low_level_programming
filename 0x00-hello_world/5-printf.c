4c95c0906c19cf61b6b901e265c84261a26e1620
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-02-16 04:11:39 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
- * main - Hello, printf
- *
- * Return: Always 0(Success)
+ * main - prints "with proper grammer, but the outcome is a piece of art,"
+ * Return: Always 0.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of varous types of computer it is compiled
+ * Return 0 (Sucess)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+
