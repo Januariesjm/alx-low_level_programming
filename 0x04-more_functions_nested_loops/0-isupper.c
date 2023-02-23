21ef5b8ef02f9f1359c56e20bdc8fa7f4137d20e
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-02-23 06:01:59 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - checks if letter is uppercase character
+* @c: Variable text
+* Return: 1 for uppercase letter or 0 for lowercase letter.
+*/
+int _isupper(int c)
+{
+
+
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
+{
+ return (0);
+}
+
+}
